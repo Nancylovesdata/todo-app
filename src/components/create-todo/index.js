15f6328e7a76d876a103e5bec083d6abd50fc2fe
Nancylovesdata <119846715+Nancylovesdata@users.noreply.github.com>
2024-02-16 16:06:29 +0000

    Completed the create todo and started the todolist

--- src/components/create-todo/index.js
+++ src/components/create-todo/index.js
@@ -1,17 +1,35 @@
 import React from "react";
-import styles from "./index.module.css"
+import styles from "./index.module.css";
 
 function CreateTodo(){
-  const[todo, setTodo] = React.useState("");
+  let todo;
+
+  function collectInput(event){
+    todo= event.target.value;
+    
+  }
+
+  function saveTodo(){
+    // Get existing list of todos from local storage
+    //Note || means or, so if else can can be replaced by line 15
+    let todos= JSON.parse(localStorage.getItem("TODO_KEY")) || [];
+    // Add new todo to existing list of todos
+    todos.push(todo);
+    // Set all todos in local storage
+    localStorage.setItem("TODO_KEY", JSON.stringify (todos));
+    // console.log(todo)
+
+  }
+  
     return (
       <section className={styles.createTodoSection}>
         <input
-         onChange={function(event){
-          console.log(event.target.value);
-         }}
+         onChange={ collectInput}
+         
           className={styles.createTodoInput} 
-         placeholder="Start typing..." />
-        <button>Create</button>
+         placeholder="Start typing..." 
+         />
+        <button onClick= {saveTodo}>Create</button>
       </section>  
     );
 
--- src/components/todo-list/index.js
+++ src/components/todo-list/index.js
@@ -1,11 +1,24 @@
+import React from "react";
 
 
 function TodoList(){
-    return (
-<ul> 
-    <li> Complete online javascript course</li>
-    <li>Jog around the park 3 times</li>
+   // let todos [];
+   const [todos,setTodos] = React.useState([]);
+    function getTodos(){
+        // Get all todos from local storage and store it.
+        let todos= JSON.parse(localStorage.getItem("TODO_KEY")) || [];
 
+     //Update React State   
+     setTodos(todos);
+    }
+
+
+React.useEffect(getTodos, []);
+    return(
+        <ul> 
+        {todos.map(function(todo, index){
+            return <li key= {index}>{todo}</li>
+            })}
 </ul>
     );
 }
