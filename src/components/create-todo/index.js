7f13c5c0499c8b2b61f535cde88638ee0c7402e8
Nancylovesdata <nancyeofreeman@gmail.com>
2024-03-13 16:30:06 +0000

    Deleted the create todo loacal storage

--- src/components/create-todo/index.js
+++ src/components/create-todo/index.js
@@ -1,39 +1,44 @@
 // import React from "react";
 import styles from "./index.module.css";
-import { useLocalStorage } from "usehooks-ts";
+
 import { useState } from "react";
 
 function CreateTodo() {
-  const [todos, setTodos] = useLocalStorage("TODO_KEY", []);
   const [todo, setTodo] = useState("");
-  const saveTodos = () => {
-// Save all todos
+  const saveTodos = async () => {
+    // Post  all todos
+    const response = await fetch("http://localhost:4000/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        title: todo,
+      }),
+      headers: {
+        "content-Type": "application/json",
+      },
+    });
 
-setTodos([...todos,todo]);
-// Wipe the input box
-setTodo("");
-}
-  
+    const data = await response.json();
+    console.log(data);
+    // Wipe the input box
+    setTodo("");
+  };
 
   return (
     <section className={styles.createTodoSection}>
       <input
-      value={todo}
+        value={todo}
         //gGetting the enter key to work in the todoapp
-        onKeyDown={event => event.key === "Enter" && saveTodos([...todos,todo])}
-      
+        onKeyDown={(event) => event.key === "Enter" && saveTodos()}
         // arrow function helps you to keep a compressed way of doing things
-        onChange={event => setTodo(event.target.value)}
-
+        onChange={(event) => setTodo(event.target.value)}
         className={styles.createTodoInput}
         placeholder="Start typing..."
       />
-      <button className={`btn ${styles.btn}` }
-        onClick={() => saveTodos()}
-      >Create</button>
+      <button className={`btn ${styles.btn}`} onClick={() => saveTodos()}>
+        Create
+      </button>
     </section>
   );
-
 }
 
 export default CreateTodo;
